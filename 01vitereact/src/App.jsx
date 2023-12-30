9efa95c667fa27f961f150e9d1632e8e5a5dd844
Abhishek Kumar Singh <royalabhishekbabu321@gmail.com>
2023-12-30 05:42:17 +0530

    custom jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,13 @@
 import First from "./first"
 
 function App() {
- 
+ const username = "Abhishek Singh"
 
   return (
     <>
       <First/>
-      <h1>Learn react</h1>
-      <p>teat para</p>
+      <h1>Learn react {username}</h1>
+      <p>test para</p>
     </>
     
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp () {
+    return (
+        <div>
+            <h1>Custom App!</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "thor"
+
+const reactElement = React.createElement('a',{href: 'https://google.com', target: '_blank'},
+'Click me to visit google',
+anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    // <MyApp />
+    // ReactElement
+    // anotherElement
+    reactElement
   
 )
